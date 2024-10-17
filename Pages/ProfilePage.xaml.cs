8f75418e91072e8629935682fac96bdb005e1f52
Md Mosabbir Alam <ibs.mdmosabbir@gmail.com>
2024-10-17 18:54:36 +0530

    Half Profile Screen Code Added

--- Pages/ProfilePage.xaml.cs
+++ Pages/ProfilePage.xaml.cs
@@ -1,9 +1,29 @@
+using CommunityToolkit.Maui.Views;
+using FoodNinja.Services;
+using FoodNinja.ViewModel;
+
 namespace FoodNinja.Pages;
 
 public partial class ProfilePage : ContentPage
 {
+	private FirebaseManager firebaseManager;
 	public ProfilePage()
 	{
 		InitializeComponent();
+		firebaseManager = new FirebaseManager();
+		this.BindingContext = new ProfilePageViewModel(Navigation, firebaseManager);
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        //await this.ShowPopupAsync(new LogoutPopup());
+    }
+    private async void AddPaymentButton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AddPaymentMethodPage());
+    }
+
+ 
+  
+
 }
---
+++ ViewModel/ProfilePageViewModel.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Firebase.Auth;
+using FoodNinja.Model;
+using FoodNinja.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodNinja.ViewModel
+{
+    public partial class ProfilePageViewModel:ObservableObject
+    {
+        #region Fields
+        public List<string> Sections => new List<string> { "Profile", "Order Detail" };
+        private FirebaseManager firebaseManager = new FirebaseManager();
+        public INavigation Navigation { get; }
+
+        [ObservableProperty]
+        private bool isOpenCustom;
+
+        [ObservableProperty]
+        private string userId;
+
+        [ObservableProperty]
+        private UserDataModel userData;
+
+        [ObservableProperty]
+        private bool isLoading;
+
+        [ObservableProperty]
+        private string fullName;
+
+        public Action TriggerAnimation { get; set; }
+        #endregion
+
+        public ProfilePageViewModel(INavigation navigation, FirebaseManager _firebaseManager)
+        {
+            Navigation = navigation;
+            firebaseManager = _firebaseManager;
+            LoadUserData();
+        }
+        private async void LoadUserData()
+        {
+            await FetchUserDataAsync();
+        }
+
+        #region Methods
+        public async Task FetchUserDataAsync()
+        {
+            UserId = Preferences.Get("LocalId", string.Empty);
+            IsLoading = true;
+            var response = await firebaseManager.GetUserDataAsync(UserId);
+            if (response != null)
+            {
+                UserData = response;
+                FullName = UserData.FirstName + " " + UserData.LastName;
+            }
+            IsLoading = false;
+
+            TriggerAnimation?.Invoke();
+        }
+        #endregion
+    }
+}
